2b2d575c8bd3dbf09efe5079bb16a87d39a7684f
evgen <gorkoune@gmail.com>
2023-05-26 19:44:19 +0300

    Initial commit

---
+++ timeservice/src/main/java/ru/mirea/gorkoun/timeservice/MainActivity.java
@@ -0,0 +1,64 @@
+package ru.mirea.gorkoun.timeservice;
+
+import static android.content.ContentValues.TAG;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.Socket;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import ru.mirea.gorkoun.timeservice.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private final String host = "time.nist.gov"; // или time-a.nist.gov
+    private final int port = 13;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GetTimeTask timeTask = new GetTimeTask();
+                timeTask.execute();
+            }
+        });
+    }
+    private class GetTimeTask extends AsyncTask<Void, Void, String> {
+        @Override
+        protected String doInBackground(Void... params) {
+            String timeResult = "";
+            try {
+                Socket socket = new Socket(host, port);
+                BufferedReader reader = SocketUtils.getReader(socket);
+                reader.readLine(); // игнорируем первую строку
+                timeResult = reader.readLine(); // считываем вторую строку
+                Log.d(TAG,timeResult);
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return timeResult;
+        }
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            String date = result.substring(6,23);
+            //String pattern = "yyyy-MM-dd";
+            //SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+            binding.textView.setText(date);
+        }
+    }
+}
